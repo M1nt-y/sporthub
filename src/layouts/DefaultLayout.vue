035f5edfea1a0eaf664ffc8da811e97d9658fe14
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-10-08 14:30:27 +0300

    added header component

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,12 @@
 <template>
-  <!-- Header.. etc components -->
+  <TheHeader/>
   <main class="main">
     <slot />
   </main>
 </template>
 
 <script setup lang="ts">
-
+import TheHeader from '@/components/Base/TheHeader.vue'
 </script>
 
 <style scoped lang="stylus">
---
+++ src/views/SignupView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="auth-wrapper"></div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="stylus">
+
+</style>
