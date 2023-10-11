5cb6b6e0f61a5abb2a01962bc2d716a8df52ecb5
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-10-11 21:04:15 +0300

    added profile dropdown (without animation) and sign out logic

--- src/components/Base/TheHeader.vue
+++ src/components/Base/TheHeader.vue
@@ -23,18 +23,50 @@
         <NotificationIcon/>
       </div>
 
-      <div class="header__link" v-if="isLoggedIn">Video</div>
+      <div class="header__link" v-if="user && user.isBusiness">Video</div>
 
-      <div class="header__link" v-if="isLoggedIn">Store</div>
+      <div class="header__link" v-if="user && user.isBusiness">Store</div>
 
-      <div class="header__profile" v-if="isLoggedIn">
-        <div class="header__profile-image"></div>
+      <div class="header__profile" v-if="currentUser">
+        <div class="header__profile-bar" @click.stop="displayLinks = !displayLinks">
+          <div class="header__profile-image">
+            <img v-if="user && user.photo.link" :src="user.photo.link" alt="">
+          </div>
 
-        <div class="header__profile-link">Profile</div>
+          <div class="header__profile-text">Profile</div>
+        </div>
+
+        <div class="header__profile-dropdown" v-if="displayLinks" @click.stop>
+          <div class="header__profile-link">
+            <div class="header__profile-icon">
+              <EditIcon/>
+            </div>
+
+            Edit profile
+          </div>
+
+          <div class="header__profile-link" v-if="user && !user.isBusiness">
+            <div class="header__profile-icon">
+              <BusinessIcon/>
+            </div>
+
+            Switch to business account
+          </div>
+
+          <div class="header__profile-line"/>
+
+          <div class="header__profile-link" @click="handleSignOut">
+            <div class="header__profile-icon">
+              <LogoutIcon/>
+            </div>
+
+            Log out
+          </div>
+        </div>
       </div>
 
       <RouterLink to="/login" v-else>
-        <TheButton text="Sign In" :padding="12" :width="120" />
+        <TheButton text="Sign In" :padding="12" :width="120"/>
       </RouterLink>
 
     </div>
@@ -42,14 +74,51 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
+import {storeToRefs} from 'pinia'
+import {useAuthStore} from '@/stores/auth'
+import {getAuth, onAuthStateChanged, signOut} from 'firebase/auth'
+import {collection, getDocs, getFirestore, query, where} from 'firebase/firestore'
 import LogoIcon from '@/assets/icons/Header/LogoIcon.vue'
+import EditIcon from '@/assets/icons/Header/EditIcon.vue'
 import SearchIcon from '@/assets/icons/Header/SearchIcon.vue'
+import LogoutIcon from '@/assets/icons/Header/LogoutIcon.vue'
+import BusinessIcon from '@/assets/icons/Header/BusinessIcon.vue'
 import NotificationIcon from '@/assets/icons/Header/NotificationIcon.vue'
 import TheButton from '@/components/UI/Buttons/TheButton.vue'
+import type {UserType} from '@/types/types'
 
 
-const isLoggedIn = ref(true)
+const auth = getAuth()
+const db = getFirestore()
+const authStore = useAuthStore()
+const {currentUser, user} = storeToRefs(authStore)
+const usersRef = collection(db, 'publicUsers')
+
+const displayLinks = ref(false)
+
+onMounted(async () => {
+  document.addEventListener('click', () => {
+    displayLinks.value = false
+  })
+
+  await onAuthStateChanged(auth, async (userRef) => {
+    if (userRef && !user.value) {
+      const q = query(usersRef, where("email", "==", userRef.email))
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        user.value = doc.data() as UserType
+      });
+    } else if (!userRef) {
+      user.value = null
+    }
+  });
+})
+
+async function handleSignOut() {
+  currentUser.value = null
+  await signOut(auth)
+}
 </script>
 
 <style lang="stylus">
@@ -131,6 +200,7 @@ const isLoggedIn = ref(true)
   &__link
     color #BBB
     font-size 16px
+    cursor pointer
     margin-left 24px
     padding 14px 10px
 
@@ -138,22 +208,80 @@ const isLoggedIn = ref(true)
       display none
 
   &__profile
-    display flex
     margin-left 24px
-    padding 7px 10px
-    align-items center
+    position relative
 
     @media screen and (max-width 576px)
       display none
 
+    &-bar
+      display flex
+      cursor pointer
+      padding 7px 10px
+      user-select none
+      align-items center
+
     &-image
       width 24px
       height 24px
+      overflow hidden
       background wheat
       border-radius 24px
 
-    &-link
+      img
+        width 100%
+        height 100%
+        object-fit cover
+
+    &-text
+      padding-left 10px
+
+    &-text, &-link
       color #BBB
       font-size 16px
-      padding 14px 0 14px 10px
+
+    &-link
+      display flex
+      cursor pointer
+      align-items center
+      margin-bottom 16px
+      transition color 0.3s ease
+
+      &:last-child
+        margin-top 16px
+        margin-bottom 0
+
+      &:hover
+        color #FFF
+
+        svg
+          path
+            fill #FFF
+
+    &-icon
+      margin-right 8px
+
+      svg
+        path
+          transition fill 0.3s ease
+
+    &-line
+      width 100%
+      height 1px
+      background #111
+
+    &-dropdown
+      display flex
+      padding 16px
+      background #222
+      min-width 200px
+      min-height 111px
+      max-height 160px
+      border-radius 8px
+      position absolute
+      flex-direction column
+      justify-content space-between
+      top 42px
+      right 0
+
 </style>
