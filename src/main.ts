5863f43f1a284343bb4e1329a5b49ea8dfd76bad
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-10-11 21:05:04 +0300

    added pinia-plugin-persistedstate

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/fonts/fontface.styl'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { VueFire, VueFireAuth } from 'vuefire'
 
 import App from './App.vue'
@@ -9,8 +10,10 @@ import router from './router'
 import firebaseApp from './firebase'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(VueFire, {
     firebaseApp,
