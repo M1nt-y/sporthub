e8f3c3dd6508ca75d3ef2f2813b0774a08767916
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-10-11 20:30:40 +0300

    remove unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -12,26 +12,13 @@ import AppLayout from "@/layouts/AppLayout.vue";
 <style lang="stylus">
 
 *
-  box-sizing border-box
-
-html, body, div, span, applet, object,
-h1, h2, h3, h4, h5, h6, p, blockquote,
-pre, a, abbr, acronym, address, big,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center, dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend, caption,
-table, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-menu, nav, ruby, iframe, video, audio, time,
-code, mark, section, summary, output, cite
   margin 0
   padding 0
   border 0
   font-size 100%
-  font-family Uto, sans-serif
+  box-sizing border-box
   vertical-align baseline
+  font-family Uto, sans-serif
 
 article, aside, details, figcaption, figure,
 footer, header, hgroup, menu, nav, section
